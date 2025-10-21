75c05b8738b452839f7ce6cb381f3ab2938d505b
wujoseph <wujoseph12345@gmail.com>
2025-10-21 17:49:56 +0800

    update

---
+++ command.sh
@@ -0,0 +1,9 @@
+python render_multiview.py  /home/wujoseph/github/articulate-partfield/data/partnet-mobility-v0/dataset/1011/textured_objs /home/wujoseph/github/articulate-partfield/data/partnet-mobility-v0/dataset/1011/multiview
+
+
+
+blender --background --python render_multiview.py -- /home/wujoseph/github/articulate-partfield/data/partnet-mobility-v0/dataset/1011/textured_objs /home/wujoseph/github/articulate-partfield/data/partnet-mobility-v0/dataset/1011/multiview
+
+
+blender --background --python render_cycle.py -- /home/wujoseph/github/articulate-partfield/data/partnet-mobility-v0/dataset/1011/textured_objs /home/wujoseph/github/articulate-partfield/data/partnet-mobility-v0/dataset/1011/multiview/cycle
+
